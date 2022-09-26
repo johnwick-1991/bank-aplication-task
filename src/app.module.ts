6b5ef6b6b442a826db314d0de96b7ef809dda5a1
giorgi <gjanashia@unisens.ge>
2022-09-26 14:09:55 +0400

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
-import { Usersmodule} from './Users/controllers/usermodule/user.module';
-import { UserController } from './Users/controllers/user.controller';;
+import { Usersmodule } from './Users/controllers/usermodule/user.module';
+import { UserController } from './Users/controllers/user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import entities from './typeorm/index';
 
 @Module({
-  imports: [Usersmodule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '@@karlito1991@@',
-    database: 'bankappusers',
-    entities,
-    synchronize:true,
-  }) ],
+  imports: [
+    Usersmodule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '@@karlito1991@@',
+      database: 'bankappusers',
+      entities,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
